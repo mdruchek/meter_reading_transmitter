feab4757056e1ead8b63c14df001429b5c2906dc
Mike <mdruchek@yandex.ru>
2025-12-02 18:07:53 +0300

    create CAMPAIGN_REGISTRY in campaigns.py

--- src/meter_reading_transmitter/campaigns.py
+++ src/meter_reading_transmitter/campaigns.py
@@ -1,55 +1,12 @@
-
-class CampaignInterface(ABC):
-    name: str
-
-    @staticmethod
-    @abstractmethod
-    def make_campaign(
-        personal_account: str,
-    ) -> CampaignData:
-        ...
-
-
-class KVCCampaign(CampaignInterface):
-    name = "КВЦ"
-
-    @staticmethod
-    def get_active_regions() -> list[dict]:
-        url = "https://send.kvc-nn.ru/api/ControlIndications/GetActiveCtrRegions"
-        response = requests.post(url)
-        return response.json()
-
-    @staticmethod
-    def get_locations_for_region(region_id: int) -> list[dict]:
-        url = "https://send.kvc-nn.ru/api/ControlIndications/GetLocationsForRegion"
-        params = {"idRegion": region_id}
-        response = requests.post(url, params=params)
-        return response.json()
-
-    @staticmethod
-    def get_abonent_info(location_for_region: dict) -> dict:
-        url = "https://send.kvc-nn.ru/api/ControlIndications/GetAbonentInfo"
-        response = requests.post(url, json=location_for_region)
-        return response.json()
-
-    @staticmethod
-    def make_campaign(
-        region_id: int,
-        region_name: str,
-        personal_account: str,
-    ) -> CampaignData:
-        return CampaignData(
-            name="квц",
-            region_id=region_id,
-            region_name=region_name,
-            personal_account=personal_account,
-        )
-
-# campaigns.py
 from abc import ABC, abstractmethod
 import requests
 
 from .models import CampaignData
+
+
+CAMPAIGN_REGISTRY: dict[str, type[CampaignInterface]] = {
+    KVCCampaign.key: KVCCampaign,
+}
 
 
 class CampaignInterface(ABC):
@@ -75,21 +32,28 @@ class KVCCampaign(CampaignInterface):
         return resp.json()
 
     @staticmethod
+    def get_locations_for_region(region_id: int) -> list[dict]:
+        url = "https://send.kvc-nn.ru/api/ControlIndications/GetLocationsForRegion"
+        params = {"idRegion": region_id}
+        response = requests.post(url, params=params)
+        return response.json()
+
+    @staticmethod
+    def get_abonent_info(location_for_region: dict) -> dict:
+        url = "https://send.kvc-nn.ru/api/ControlIndications/GetAbonentInfo"
+        response = requests.post(url, json=location_for_region)
+        return response.json()
+
+    @staticmethod
     def make_campaign(
         region_id: int,
         region_name: str,
         personal_account: str,
     ) -> CampaignData:
         return CampaignData(
-            name="квц",
+            key='title',
+            title="КВЦ",
             region_id=region_id,
             region_name=region_name,
             personal_account=personal_account,
         )
-
-
-# сюда позже добавишь другие кампании
-CAMPAIGN_REGISTRY: dict[str, type[CampaignInterface]] = {
-    KVCCampaign.key: KVCCampaign,
-    # "other": OtherCampaign,
-}
